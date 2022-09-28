8b9697d85d724045d87fbe1cef37780ae867c1df
romansahel <roman.sahel1987@gmail.com>
2022-09-27 17:07:02 -0700

    therit commit

--- src/Class03.java
+++ src/Class03.java
@@ -4,6 +4,7 @@ public class Class03 {
 	public static void main(String[] args) {
 		System.out.println("hey this is a git class");
 		System.out.println("hey this is a git class");
+		System.out.println("hwo are you");
 
 	}
 
